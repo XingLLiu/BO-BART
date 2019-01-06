723babe71feda7341d245b2eedc6abfdd17e8723
harrisonzhu508 <harrisonzhu5080@gmail.com>
2019-01-06 19:21:30 +0000

    add in figures and report

--- src/integrationMain.R
+++ src/integrationMain.R
@@ -9,20 +9,14 @@ source("src/references/cornerPeakFamily.R") # genz function to test
 # set number of new query points using sequential design
 source("src/BARTBQ.R")
 predictionBART <- mainBARTBQ()
-meanValueBART <- predictionBART[1]
-standardDeviationBART <- predictionBART
 
 # Bayesian Quadrature with Monte Carlo integration method
 source("src/monteCarloIntegration.R")
 predictionMonteCarlo <- monteCarloIntegrationUniform(copeak)
-meanValueMonteCarlo <- predictionMonteCarlo[1]
-standardDeviationMonteCarlo <- predictionMonteCarlo[2]
 
 # Bayesian Quadrature with Gaussian Process
 source("src/GPBQ.R")
 predictionGPBQ <- computeGPBQ(dim, epochs = 400, N=10, FUN = copeak)
-meanValueGP <- predictionGPBQ[1]
-standardDeviationGP <- predictionGPBQ[2]
 
 
 # Exact integral of genz function in hypercube [0,1]^dim
@@ -32,6 +26,20 @@ real <- adaptIntegrate(copeak,lowerLimit = rep(0,dim), upperLimit = rep(1,dim))
 percentageErrorBART <- meanValueBART - real[[1]]
 percentageErrorMonteCarlo <- meanValueMonteCarlo - real[[1]]
 percentageErrorGP <- meanValueGP - real[[1]]
+
+# 1. Open jpeg file
+jpeg("report/Figures//convergenceCopeak.jpg", width = 700, height = 583)
+# 2. Create the plot
+plot(x = log(c(1:400)), y = log(standardDeviationMonteCarlo$standardDeviation2),
+     pch = 16, frame = FALSE, type = "l",
+     xlab = "number of samples N", ylab = "log standard deviation", col = "blue",
+     main = "Convergence of methods: log(sd) vs log(N) using Copeak")
+lines(x = log(c(1:400)), log(predictionBART$standardDeviation), type = 'l', col = "red")
+lines(x = log(c(1:400)), log(predictionGPBQ$standardDeviationGP), type = 'l', col = "green")
+legend("topleft", legend=c("MC Integration", "BART BQ", "GP BQ"),
+       col=c("blue", "red", "green"), lty=1:2, cex=0.8)
+# 3. Close the file
+dev.off()
 
 
 #[5, 4, 3, 2, 1.5, 1.3, 1.2, 1.1]
