e24695d711dfe1d24ed88741476d4c100f9af2b3
OoAugust <37644581+OoAugust@users.noreply.github.com>
2019-01-10 12:55:10 +0000

    Create analytical solution.R

---
+++ src/analytical solution.R
@@ -0,0 +1,28 @@
+analytical_sol <- function (FUN, dim){
+  
+  #Analytical Solution of Genz, for cont, gaussian, prpeak and disc only 
+  if (FUN == "disc"){
+    
+    if (dim == 1){
+      print ("disc undefined for dimension = 1")
+      
+      break
+      
+    }
+    
+    f <- function (x) exp(5 * x)
+    
+    result <- (( adaptIntegrate(f,lowerLimit = rep(0, dim),upperLimit = rep(0.5, dim))[[1]] ) ^ (2)) *
+                ( adaptIntegrate(f,lowerLimit = rep(0, dim),upperLimit = rep(1, dim))[[1]] ) ^ (dim - 2)
+    
+    
+  }else{
+    
+    result <- (adaptIntegrate(FUN,lowerLimit = rep(0,3),upperLimit = rep(1,3))[[1]]) ^ (dim)
+    
+  }
+  
+  return (result)
+  
+  
+}
