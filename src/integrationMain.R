8ec93b8df3c17c2dc0330521c47fef37882ee182
harrisonzhu508 <harrisonzhu5080@gmail.com>
2019-01-11 17:36:48 +0000

    Fix some plots. Ready to submit to cluster server

--- src/integrationMain.R
+++ src/integrationMain.R
@@ -105,31 +105,28 @@ print("Begin Plots")
 # 1. Open jpeg file
 jpeg("../report/Figures/%s/convergenceMean%s%sDimensions.jpg" %--% c(whichGenz, genzFunctionName, dim), width = 700, height = 583)
 # 2. Create the plot
+par(mfrow = c(1,2), pty = "s")
 plot(x = c(1:num_iterations), y = predictionMonteCarlo$meanValueMonteCarlo,
-     pch = 16, frame = FALSE, type = "l",
+     pch = 16, type = "l",
      xlab = "Number of epochs N", ylab = "Integral approximation", col = "blue",
-     main = "Convergence of methods: mean vs N \nusing %s with %s epochs" %--% c(genzFunctionName, num_iterations, dim),
-     ylim = c(0, real + real), 
-     lty =1
+     main = "Convergence of methods: mean vs N \nusing %s with %s epochs in %s dim" %--% c(genzFunctionName, num_iterations, dim),
+     ylim = c(-real, real + real), 
+     lty = 1,
+     xaxs="i", yaxs="i"
      )
 lines(x = c(1:num_iterations), predictionBART$meanValueBART, type = 'l', col = "red", lty = 1)
 lines(x = c(1:num_iterations), predictionGPBQ$meanValueGP, type = 'l', col = "green", lty = 1)
 abline(a = real, b = 0, lty = 4)
 legend("topleft", legend=c("MC Integration", "BART BQ", "GP BQ", "Actual"),
        col=c("blue", "red", "green", "black"), cex=0.8, lty = c(1,1,1,1))
-# 3. Close the file
-dev.off()
 
-
-print(predictionMonteCarlo$standardDeviationMonteCarlo)
-# 1. Open jpeg file
-jpeg("../report/Figures/%s/convergence%sStandardDeviation%sDimensions.jpg" %--% c(whichGenz, genzFunctionName, dim), width = 700, height = 583)
 # 2. Create the plot
 plot(x = log(c(2:num_iterations)), y = log(predictionMonteCarlo$standardDeviationMonteCarlo[-1]),
-     pch = 16, frame = FALSE, type = "l",
+     pch = 16, type = "l",
      xlab = "Number of epochs N", ylab = "Log standard deviation", col = "blue",
      main = "Convergence of methods: log(sd) vs log(N) \nusing %s with %s epochs in %s dim" %--% c(genzFunctionName, num_iterations, dim),
-     lty = 1)
+     lty = 1,
+     xaxs="i", yaxs="i")
 lines(x = log(c(2:num_iterations)), log(predictionBART$standardDeviationBART[-1]), type = 'l', col = "red", lty = 1)
 lines(x = log(c(2:num_iterations)), log(predictionGPBQ$standardDeviationGP[-1]), type = 'l', col = "green", lty = 1)
 legend("topleft", legend=c("MC Integration", "BART BQ", "GP BQ"),
