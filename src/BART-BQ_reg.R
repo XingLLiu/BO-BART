52d2bb95b0a7d9cbc94c97f6c279c16d4413581d
RuyaKang <rk2616@ic.ac.uk>
2019-01-15 00:06:42 +0000

    debugging, wrong prediction

--- src/BART-BQ_reg.R
+++ src/BART-BQ_reg.R
@@ -1,5 +1,6 @@
 library(dbarts)
 library(data.tree)
+library(Rfast)
 source("/Users/christine798/Documents/GitHub/BO-BART/src/BARTBQ.R")
 
 rescale <- function(myData) {
@@ -17,7 +18,57 @@ rescale <- function(myData) {
 
 }
 
-singleTreePrediction <- function(tree, x, cutPoints) {
+scale <- function(mu, max, min) {
+
+    mu <- (mu + 0.5) * (max - min) + min
+
+    return(mu)
+
+}
+
+fillProbabilityForNode_update <- function(oneTree, cutPoints, cut) 
+# Drop data set into the tree and assign them to different nodes 
+{
+  if ( !is.null(oneTree$leftChild) ) {
+    
+    decisionRule <- cutPoints[[oneTree$splitVar]][oneTree$splitIndex]
+
+    oneTree$leftChild$probability <- (decisionRule - cut[1, oneTree$splitVar]) / (cut[2, oneTree$splitVar] - cut[1, oneTree$splitVar])
+    #cat("left prob", oneTree$leftChild$probability, "\n")
+    oneTree$rightChild$probability <- (cut[2, oneTree$splitVar] - decisionRule) / (cut[2, oneTree$splitVar] - cut[1, oneTree$splitVar])
+    #cat("right prob", oneTree$rightChild$probability, "\n")
+
+    range <- cut[, oneTree$splitVar]
+    cut[, oneTree$splitVar] <- c(range[1], decisionRule)
+    
+    # if (cut[1, oneTree$splitVar] == cut[2, oneTree$splitVar]) {
+        
+    #     print(c("left", cut[, oneTree$splitVar]))
+
+    # }
+    
+    fillProbabilityForNode(oneTree$leftChild, cutPoints, cut)
+    
+    cut[, oneTree$splitVar] <- c(decisionRule, range[2])
+
+    # if (cut[1, oneTree$splitVar] == cut[2, oneTree$splitVar]) {
+        
+    #     print(c("right", cut[, oneTree$splitVar]))
+
+    # }
+    
+    fillProbabilityForNode(oneTree$rightChild, cutPoints, cut)
+    
+  } else if( is.null(oneTree$probability) ) {
+
+    oneTree$probability <- 1
+
+  }
+  
+  return (oneTree)
+}
+
+singleTreePrediction <- function(tree, x, cutPoints, max, min) {
 # compute a prediction given by one tree in one posterior draw
 
     # return prediction when reach terminal node
@@ -25,86 +76,89 @@ singleTreePrediction <- function(tree, x, cutPoints) {
         
         # decision rule on specific element
         decisionRule <- cutPoints[[tree$splitVar]][tree$splitIndex]
-
+        
         # locate datapoint to next child
         if (x[tree$splitVar] <= decisionRule) {
 
-            singleTreePrediction(tree$leftChild, x, cutPoints)
-
+            singleTreePrediction(tree$leftChild, x, cutPoints, max, min)
+           
         } else {
             
-            singleTreePrediction(tree$rightChild, x, cutPoints)
-       
+            singleTreePrediction(tree$rightChild, x, cutPoints, max, min)
+            
         }
 
     } else {
-        
-        # return single tree prediction
-        return(tree$terminal_probability * tree$mu)
+
+        #cat("prob", tree$terminal_probability, "\n")
+        #cat("mu", tree$mu, "\n")
+        #return single tree prediction
+        #return(tree$terminal_probability * scale(tree$mu, max, min))
+        return(scale(tree$mu, max, min))
 
     }
 
 }  
 
-
-singlePosteriorPrediction <- function(drawNum, model, x, nTree) {
+singlePosteriorPrediction <- function(drawNum, model, x, nTree, cut, max, min) {
 # compute prediction given by a posterior draw (first quadrature)
 
     cutPoints <- dbarts:::createCutPoints(model$fit)
-    cut <- array(c(0, 1), c(2, dim))
 
-    posteriorPrediction <- NULL
+    singlePosteriorPrediction <- rep(NULL, nTree)
 
     for (i in 1:nTree) {
   
         treeList <- getTree(model$fit, 1, drawNum, i)
-  
+        
         tree <- FromListSimple(treeList) 
-  
+        
         #Modify tree by the functions written above
-        tree <- fillProbabilityForNode(tree, cutPoints, cut)
+        tree <- fillProbabilityForNode_update(tree, cutPoints, cut)
         tree <- terminalProbabilityStore(tree)
-
-        singlePosteriorPrediction <- c(posteriorPrediction, singleTreePrediction(tree, x, cutPoints))
-
+    
+        singlePosteriorPrediction[i] <- singleTreePrediction(tree, x, cutPoints, max, min)
+    
     }
     
     return(mean(singlePosteriorPrediction))
 
 }
 
-posteriorPrediction <- function(testNum, model, test, nTree) {
-
+posteriorPrediction <- function(testNum, model, test, nTree, cut, max, min) {
+    
     x <- test[testNum, ]
     
     nDraw <- dim(model$fit$state[[1]]@savedTreeFits)[3]
-    drawNum <- seq(1, nDraw, length.out=nDraw)
+    drawNum <- seq(1, nDraw, length.out = nDraw)
 
     #Extra variables
-    var <- list(model, x, nTree)
-  
+    var <- list(model, x, nTree, cut, max, min)
+    
     #Calculate integration over all trees in the draw by mapply
-    posteriorPrediction <- mapply(singlePosteriorPrediction, drawNum, MoreArgs=var)
+    posteriorPrediction <- mapply(singlePosteriorPrediction, drawNum, MoreArgs = var)
 
-    return(sum(posteriorPrediction))
+    return(mean(posteriorPrediction))
 
 }
 
-
-
-bartBQPredict <- function(model, test) {
+bartBQPredict <- function(model, train, test, max, min) {
 # predict response to a new datapoint with BART-BQ
 
+    cut_origin <- rbind(colMins(as.matrix(train[, 1:(ncol(train)-1)]), value = TRUE), colMaxs(as.matrix(train[, 1:(ncol(train)-1)]), value = TRUE))
+    #cut_origin <- array(c(0, 1), c(2, 2))
+    
+    #print(c("cut", cut))
     nTree <- dim(model$fit$state[[1]]@savedTreeFits)[2]
     
     nTest <- nrow(test)
-    testNum <- seq(1, nTest, length.out=nTest) 
-  
+    testNum <- seq(1, nTest, length.out = nTest) 
+
     #Extra variables
-    var <- list(model, test, nTree)
-  
+    var <- list(model, test, nTree, cut_origin, max, min)
+
     #Calculate integration over all trees in the draw by mapply
-    prediction <- mapply(posteriorPrediction, testNum, MoreArgs=var)
+    prediction <- mapply(posteriorPrediction, testNum, MoreArgs = var)
 
     return(prediction)
 
--- src/regression.R
+++ src/regression.R
@@ -12,21 +12,32 @@ n <- 50
 
 highDimMSE <- c("benchmark", "BARTBQ")
 
-for (dim in c(2, 3, 5, 10, 20)) {
+for (dim in c(2)) {
    
    set.seed(1223)
    col <- c(sample(c(1:25, 27, 28), dim), 26)
    train <- netTrain[sample(nrow(netTrain), n, replace = FALSE), col]
    test <- netTest[sample(nrow(netTest), n/2, replace = FALSE), col]
 
-   bartBench <- bart(train[, 1:dim], train[, dim+1], keeptrees=TRUE, keepevery=20L, nskip=1000, ndpost=1000, ntree=50, k = 5)
-   bart <- bart(rescale(train[, 1:dim]), train[, dim+1], keeptrees=TRUE, keepevery=20L, nskip=1000, ndpost=1000, ntree=50, k = 5)
-   
+   ymax <- max(train[, dim+1])
+   ymin <- min(train[, dim+1])
+
+   # trainX <- cbind(sample(1000, 100)/1000, sample(2000, 100)/2000)
+   # trainY <- sin(trainX[, 1] + trainX[, 2])
+   # trainFull <- cbind(trainX, trainY)
+
+   # testX <- cbind(sample(1000, 10)/1000, sample(2000, 10)/2000)
    print(c("dim = ", dim))
 
-   benchPred <- colMeans(predict(bartBench, test[, 1:dim]))
-   pred <- bartBQPredict(bart, rescale(test[, 1:dim]))
+   bart <- bart(rescale(train[, 1:dim]), train[, dim+1], keeptrees=TRUE, keepevery=20L, nskip=1000, ndpost=1000, ntree=50, k = 5)
+   pred <- bartBQPredict(bart, train, rescale(test[, 1:dim]), ymax, ymin)
 
+   #bart <- bart(trainFull[, 1:dim], trainFull[, dim+1], keeptrees=TRUE, keepevery=20L, nskip=1000, ndpost=1000, ntree=50, k = 2)
+
+   bartBench <- bart(train[, 1:dim], train[, dim+1], keeptrees=TRUE, keepevery=20L, nskip=1000, ndpost=1000, ntree=50, k = 5)
+   benchPred <- colMeans(predict(bartBench, test[, 1:dim]))
+   #pred <- bartBQPredict(bart, trainFull, testX[, 1:dim], max(trainFull[, dim+1]), min(trainFull[, dim+1]))
+   
    benchMSE <- mse(test[, dim+1], benchPred)
    MSE <- mse(test[, dim+1], pred)
 
