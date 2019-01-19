590f78ccd4d664b21dc2437b80874b1183743d6e
RuyaKang <rk2616@ic.ac.uk>
2019-01-19 13:24:05 +0000

    plot changes

--- data/clean_data.R
+++ data/clean_data.R
@@ -1,23 +1,16 @@
-#setwd("~/Documents/GitHub/BO-BART/src/meanPopulationStudy/")
+setwd("~/Documents/GitHub/BO-BART/src/meanPopulationStudy/")
+
+library(mlogit)
+
 # read in data
-data <- read.csv("full_data.csv")
+data <- read.csv("../../data/full_data.csv")
 
-set.seed(1223)
-
-cleanData <- data[-which(is.na(data$Mobility)), ]
-cleanData <- cleanData[-which(is.na(cleanData$Employment)), ]
-cleanData <- cleanData[-which(is.na(cleanData$Own_child)), ]
-cleanData <- cleanData[-which(cleanData$Total_person_income <= 0), ]
-
-write.csv(cleanData, file = "full_data.csv")
-
-ref <- sample(4076, 2038, replace = FALSE)
-
-train <- cleanData[ref[1:1019], ]
-candidate <- cleanData[ref[1020:2038], ]
-
-write.csv(train, file = "train.csv")
-write.csv(candidate, file = "candidate.csv")
-
-Rpar(pty = "s")
+png("population.png", width = 600, height = 600)
+rpar(pty = "s")
 hist(data$Total_person_income, breaks = 30, xlab = "Income", ylab = "Frequency", main = NULL, xaxs = "i", yaxs = "i")
+dev.off()
+
+png("white.png", width = 600, height = 600)
+rpar(pty = "s")
+hist(data[data$Race == 1, ]$Total_person_income, breaks = 30, xlab = "Income", ylab = "Frequency", main = NULL, xaxs = "i", yaxs = "i")
+dev.off()
