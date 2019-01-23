f7a1b971f69895fa9e30c24da62e746eabc25d4a
XingLLiu <liuxing971015@outlook.com>
2019-01-23 15:34:40 +0000

    Minor changes

--- src/BARTBQ.R
+++ src/BARTBQ.R
@@ -185,7 +185,7 @@ BARTSequential <- function(dim, trainX, trainY, numNewTraining, FUN, ifRegressio
     # first build BART and scale mean and standard deviation
     sink("/dev/null")
     # model <- bart(trainData[,1:dim], trainData[,dim+1], keeptrees=TRUE, keepevery=20L, nskip=1000, ndpost=1000, ntree=50, k = 5)
-    model <- bart(trainData[,1:dim], trainData[,dim+1], keeptrees=TRUE, keepevery=20L, nskip=1000, ndpost=1000)
+    model <- bart(trainData[,1:dim], trainData[,dim+1], keeptrees=TRUE, keepevery=20L, nskip=1000, ndpost=2000)
     sink()
     # obtain posterior samples
     integrals <- sampleIntegrals(model, dim)
