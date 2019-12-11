9ca5c8178052138441459999006ca606b252250b
Harrison Zhu <harrisonz@ma-hbz15.ma.ic.ac.uk>
2019-12-11 21:51:06 +0000

    fix genz.R

---
+++ src/genz/genz.R
@@ -0,0 +1,409 @@
+# ================================================================
+# Genz test functions. Input must be of type Matrix
+# ================================================================
+
+library(matrixStats)
+library(MASS)
+cont <- function(xx)
+{
+  ##########################################################################
+  #
+  # CONTINUOUS INTEGRAND FAMILY
+  #
+  # Authors: Sonja Surjanovic, Simon Fraser University
+  #          Derek Bingham, Simon Fraser University
+  # Questions/Comments: Please email Derek Bingham at dbingham@stat.sfu.ca.
+  #
+  # Copyright 2013. Derek Bingham, Simon Fraser University.
+  #
+  # THERE IS NO WARRANTY, EXPRESS OR IMPLIED. WE DO NOT ASSUME ANY LIABILITY
+  # FOR THE USE OF THIS SOFTWARE.  If software is modified to produce
+  # derivative works, such modified software should be clearly marked.
+  # Additionally, this program is free software; you can redistribute it 
+  # and/or modify it under the terms of the GNU General Public License as 
+  # published by the Free Software Foundation; version 2.0 of the License. 
+  # Accordingly, this program is distributed in the hope that it will be 
+  # useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+  # of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+  # General Public License for more details.
+  #
+  # For function details and reference information, see:
+  # http://www.sfu.ca/~ssurjano/
+  #
+  ##########################################################################
+  #
+  # INPUTS:
+  #
+  # xx = c(x1, x2, ..., xd)
+  # u = c(u1, u2, ..., ud) (optional), with default value
+  #     c(0.5, 0.5, ..., 0.5)
+  # a = 150/dim^3, where dim = number of column of xx
+  #
+  ##########################################################################
+  
+  if (is.matrix(xx) == FALSE) { 
+    xx <- matrix(xx, nrow = 1)    
+  }
+
+  dim <- ncol(xx)
+  a <- rep(150/dim^3, dim)  
+  u <- rep(0.5, dim)
+
+  sum <- abs(xx-u) %*% a
+  y <- exp(-sum)
+  
+  return(y)
+}
+
+
+copeak <- function(xx)
+{
+  ##########################################################################
+  #
+  # CORNER PEAK INTEGRAND FAMILY
+  #
+  # Authors: Sonja Surjanovic, Simon Fraser University
+  #          Derek Bingham, Simon Fraser University
+  # Questions/Comments: Please email Derek Bingham at dbingham@stat.sfu.ca.
+  #
+  # Copyright 2013. Derek Bingham, Simon Fraser University.
+  #
+  # THERE IS NO WARRANTY, EXPRESS OR IMPLIED. WE DO NOT ASSUME ANY LIABILITY
+  # FOR THE USE OF THIS SOFTWARE.  If software is modified to produce
+  # derivative works, such modified software should be clearly marked.
+  # Additionally, this program is free software; you can redistribute it 
+  # and/or modify it under the terms of the GNU General Public License as 
+  # published by the Free Software Foundation; version 2.0 of the License. 
+  # Accordingly, this program is distributed in the hope that it will be 
+  # useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+  # of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+  # General Public License for more details.
+  #
+  # For function details and reference information, see:
+  # http://www.sfu.ca/~ssurjano/
+  #
+  ##########################################################################
+  #
+  # INPUTS:
+  #
+  # xx = c(x1, x2, ..., xd)
+  # u = c(u1, u2, ..., ud) (optional), with default value
+  #     c(0.5, 0.5, ..., 0.5)
+  # a = 600/dim^3, where dim = number of column of xx
+  #
+  #########################################################################
+  
+  if (is.matrix(xx) == FALSE) { 
+    xx <- matrix(xx, nrow = 1) 
+  }
+
+  dim <- ncol(xx)
+  u <- rep(0.5, dim)
+
+  a <- matrix(rep(600/dim^3, dim), ncol = 1)
+  
+  sum <- xx %*% a
+  y <- (1 + sum)^(-dim - 1)
+  
+  return(y)
+}
+
+
+disc <- function(xx)
+{
+  ##########################################################################
+  #
+  # DISCONTINUOUS INTEGRAND FAMILY
+  #
+  # Authors: Sonja Surjanovic, Simon Fraser University
+  #          Derek Bingham, Simon Fraser University
+  # Questions/Comments: Please email Derek Bingham at dbingham@stat.sfu.ca.
+  #
+  # Copyright 2013. Derek Bingham, Simon Fraser University.
+  #
+  # THERE IS NO WARRANTY, EXPRESS OR IMPLIED. WE DO NOT ASSUME ANY LIABILITY
+  # FOR THE USE OF THIS SOFTWARE.  If software is modified to produce
+  # derivative works, such modified software should be clearly marked.
+  # Additionally, this program is free software; you can redistribute it 
+  # and/or modify it under the terms of the GNU General Public License as 
+  # published by the Free Software Foundation; version 2.0 of the License. 
+  # Accordingly, this program is distributed in the hope that it will be 
+  # useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+  # of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+  # General Public License for more details.
+  #
+  # For function details and reference information, see:
+  # http://www.sfu.ca/~ssurjano/
+  #
+  ##########################################################################
+  #
+  # INPUTS:
+  #
+  # xx = c(x1, x2, ..., xd)
+  # u = c(u1, u2, ..., ud) (optional), with default value
+  #     c(0.5, 0.5, ..., 0.5)
+  # a = 100/dim^3, where dim = number of column of xx
+  #
+  #########################################################################
+  
+  if (is.matrix(xx) == FALSE) { 
+    xx <- matrix(xx, nrow = 1)  
+  }
+  
+  # Function only defined for dimension >= 2
+  dim <- ncol(xx)
+  if (dim < 2) stop("incorrect dimension. Discrete Genz function only defined for dimension >= 2") 
+  
+  u <- rep(0.5, dim)
+  a <- rep(100/dim^3, dim)
+  
+  x1 <- xx[ ,1]
+  x2 <- xx[ ,2]
+  u1 <- u[1]
+  u2 <- u[2]
+
+  xx [which ( x1 > u1 | x2 > u2), ] <- 0
+
+  sum <- xx %*% a
+  y <- exp(sum)
+  y[which (y == 1)] <- 0
+
+  
+  return(y)
+  
+}
+
+
+gaussian <- function(xx)
+{
+  ##########################################################################
+  #
+  # GAUSSIAN PEAK INTEGRAND FAMILY
+  #
+  # Authors: Sonja Surjanovic, Simon Fraser University
+  #          Derek Bingham, Simon Fraser University
+  # Questions/Comments: Please email Derek Bingham at dbingham@stat.sfu.ca.
+  #
+  # Copyright 2013. Derek Bingham, Simon Fraser University.
+  #
+  # THERE IS NO WARRANTY, EXPRESS OR IMPLIED. WE DO NOT ASSUME ANY LIABILITY
+  # FOR THE USE OF THIS SOFTWARE.  If software is modified to produce
+  # derivative works, such modified software should be clearly marked.
+  # Additionally, this program is free software; you can redistribute it 
+  # and/or modify it under the terms of the GNU General Public License as 
+  # published by the Free Software Foundation; version 2.0 of the License. 
+  # Accordingly, this program is distributed in the hope that it will be 
+  # useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+  # of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+  # General Public License for more details.
+  #
+  # For function details and reference information, see:
+  # http://www.sfu.ca/~ssurjano/
+  #
+  ##########################################################################
+  #
+  # INPUTS:
+  #
+  # xx = c(x1, x2, ..., xd)
+  # u  = c(u1, u2, ..., ud) (optional), with default value
+  #      c(0.5, 0.5, ..., 0.5)
+  # a  = 100/dim^2, where dim = number of column of xx
+  #
+  ##########################################################################
+  
+  if (is.matrix(xx) == FALSE) { 
+    xx <- matrix(xx, nrow = 1) 
+  }
+  
+  dim <- ncol(xx)
+  u=rep(0.5, dim)
+  a <- rep(100/dim^2, dim) 
+  
+  sum <- (xx - u)^2 %*% a^2
+  y <- exp(-sum)
+  
+  return(y)
+  
+}
+
+oscil <- function(xx)
+{
+  ##########################################################################
+  #
+  # OSCILLATORY INTEGRAND FAMILY
+  #
+  # Authors: Sonja Surjanovic, Simon Fraser University
+  #          Derek Bingham, Simon Fraser University
+  # Questions/Comments: Please email Derek Bingham at dbingham@stat.sfu.ca.
+  #
+  # Copyright 2013. Derek Bingham, Simon Fraser University.
+  #
+  # THERE IS NO WARRANTY, EXPRESS OR IMPLIED. WE DO NOT ASSUME ANY LIABILITY
+  # FOR THE USE OF THIS SOFTWARE.  If software is modified to produce
+  # derivative works, such modified software should be clearly marked.
+  # Additionally, this program is free software; you can redistribute it 
+  # and/or modify it under the terms of the GNU General Public License as 
+  # published by the Free Software Foundation; version 2.0 of the License. 
+  # Accordingly, this program is distributed in the hope that it will be 
+  # useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+  # of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+  # General Public License for more details.
+  #
+  # For function details and reference information, see:
+  # http://www.sfu.ca/~ssurjano/
+  #
+  ##########################################################################
+  #
+  # INPUTS:
+  #
+  # xx = c(x1, x2, ..., xd)
+  # u  = c(u1, u2, ..., ud) (optional), with default value
+  #      c(0.5, 0.5, ..., 0.5)
+  # a  = 110/dim^(5/2), where dim = number of column of xx
+  #
+  ##########################################################################
+  
+  if (is.matrix(xx) == FALSE) { 
+    xx <- matrix(xx, nrow = 1) 
+  }
+  
+  dim <- ncol(xx)
+  u <- rep(0.5, 1, dim)
+  a <- rep(110/dim^(5/2), dim) 
+  
+  sum <- xx %*% a
+  y <- cos(2 * pi * u[1] + sum)
+  
+  return(y)
+  
+}
+
+
+
+prpeak <- function(xx)
+{
+  ##########################################################################
+  #
+  # PRODUC PEAK INTEGRAND FAMILY
+  #
+  # Authors: Sonja Surjanovic, Simon Fraser University
+  #          Derek Bingham, Simon Fraser University
+  # Questions/Comments: Please email Derek Bingham at dbingham@stat.sfu.ca.
+  #
+  # Copyright 2013. Derek Bingham, Simon Fraser University.
+  #
+  # THERE IS NO WARRANTY, EXPRESS OR IMPLIED. WE DO NOT ASSUME ANY LIABILITY
+  # FOR THE USE OF THIS SOFTWARE.  If software is modified to produce
+  # derivative works, such modified software should be clearly marked.
+  # Additionally, this program is free software; you can redistribute it 
+  # and/or modify it under the terms of the GNU General Public License as 
+  # published by the Free Software Foundation; version 2.0 of the License. 
+  # Accordingly, this program is distributed in the hope that it will be 
+  # useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+  # of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+  # General Public License for more details.
+  #
+  # For function details and reference information, see:
+  # http://www.sfu.ca/~ssurjano/
+  #
+  ##########################################################################
+  #
+  # INPUTS:
+  #
+  # xx = c(x1, x2, ..., xd)
+  # u  = c(u1, u2, ..., ud) (optional), with default value
+  #      c(0.5, 0.5, ..., 0.5)
+  # a  = 600/dim^3, where dim = number of column of xx
+  #
+  ##########################################################################
+
+  if (is.matrix(xx) == FALSE) { 
+    xx <- matrix(xx, nrow = 1)  
+  }
+  
+  dim <- ncol(xx)
+  u <- rep(0.5, dim)
+  a <- rep(600/dim^3, dim)
+  
+  sum <- a^(-2) + (xx - u)^2
+  y <- rowProds(1/sum)
+  
+  return(y)
+}
+
+
+step <- function(xx, regularizer=1e-5, jump=1)
+{
+  if (is.matrix(xx) == FALSE) { 
+    xx <- matrix(xx, nrow = 1)  
+  }
+  
+  dim <- ncol(xx)
+  
+  y <- matrix(NA, nrow = dim(xx)[1])
+  # One jump
+  if (jump == 1){
+    for (i in 1:dim(xx)[1]){
+      if (sum(xx[i, ] < 0.3) == dim){
+          y[i, ] <- regularizer
+      }
+      else{
+          y[i, ] <- 1 - regularizer
+      }
+    }
+  }
+  # Two jumps
+  else if (jump == 2){
+    for (i in 1:dim(xx)[1]){
+      if (sum(xx[i, ] < 0.3) == dim){
+          y[i, ] <- regularizer
+      }
+      else if ( sum(xx[i, ] < 0.6 & xx[i, ] >= 0.3) == dim ){
+          y[i, ] <- 0.5
+      }
+      else{
+          y[i, ] <- 1 - regularizer
+      }
+    }
+  }
+
+  return(y)
+}
+
+mix <- function(xx){
+  
+  ##########################################################################
+  #
+  # MIXTURE GENZ FUNCTION
+  #
+  # yi = cont(xi) for xi <= 0.5, copeak(xi) otherwise
+  # y = sum(yi)
+  #
+  ##########################################################################
+  
+  
+  
+  if (is.matrix(xx) == FALSE) { 
+    xx <- matrix(xx, nrow = 1)  
+  }
+  
+  dim <- ncol(xx)
+  
+  yval <- function(x, dim){
+    
+    indices_0 <- which(x <= 0.5)
+    indices_1 <- which(x > 0.5)
+    
+    y0 <- sum(apply(as.matrix(x[indices_0]), 1, cont))
+    y1 <- sum(apply(as.matrix(x[indices_1]), 1, copeak))
+    
+    y <- y0+y1
+    
+    return(y)
+  }
+  # Compute Genz function values
+  y <- apply(xx, 1, yval)
+  
+  return(as.matrix(y))
+}
+
