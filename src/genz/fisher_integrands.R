4e16e7d03e8437f878dab18fe472e269880078b3
Harrison Zhu <harrisonz@ma-hbz15.ma.ic.ac.uk>
2020-02-07 17:43:10 +0000

    remove MWE

--- src/genz/fisher_integrands.R
+++ src/genz/fisher_integrands.R
@@ -22,19 +22,19 @@ fisher_function <- function(x, C, R, H, F, P) {
   return (integrand)
 }
 
-dim <- 1
-n <- 9
-C <- replicate(dim, runif(9, 0.1, 0.9))
-R <- replicate(dim, rbeta(9, 5, 2))
-H <- replicate(dim, runif(9, 0.5*exp(1), 1.5*exp(1)))
-F <- replicate(dim, runif(9, 0, 5))
-P <- replicate(dim, rbinom(9, 1, 0.5))
-trainX <- replicate(dim, runif(500, 0, 1))
-par(mfrow = c(3, 3))
-for (i in 1:9) {
-  trainY <- fisher_function(trainX, C[i,1], R[i,1], H[i,1], F[i,1], P[i,1])
-  plot(trainX[order(trainX),], trainY[order(trainX)], ty= "l")
-} 
+# dim <- 1
+# n <- 9
+# C <- replicate(dim, runif(9, 0.1, 0.9))
+# R <- replicate(dim, rbeta(9, 5, 2))
+# H <- replicate(dim, runif(9, 0.5*exp(1), 1.5*exp(1)))
+# F <- replicate(dim, runif(9, 0, 5))
+# P <- replicate(dim, rbinom(9, 1, 0.5))
+# trainX <- replicate(dim, runif(500, 0, 1))
+# par(mfrow = c(3, 3))
+# for (i in 1:9) {
+#   trainY <- fisher_function(trainX, C[i,1], R[i,1], H[i,1], F[i,1], P[i,1])
+#   plot(trainX[order(trainX),], trainY[order(trainX)], ty= "l")
+# } 
 
 
 
