837f696c49f7e73a9319530c26a07bc92394c26a
Harrison Zhu <harrisonz@ma-hbz15.ma.ic.ac.uk>
2020-02-12 23:51:22 +0000

    add model saving mechanism

--- integrationMain.R
+++ integrationMain.R
@@ -162,7 +162,7 @@ for (num_cv in 1:5) {
           "runtimeMI" = rep(MITime, num_iterations),
           "runtimeGP" = rep(GPTime, num_iterations)
   )
-  
+  results_models <- list("BART"=predictionBART, "GP"=predictionGPBQ, "MC"=predictionMonteCarlo)
   if (!sequential){
     csvName <- "results/genz/%s/%sDim%sNoSequential%s_%s.csv" %--% c(
             whichGenz, 
@@ -178,6 +178,13 @@ for (num_cv in 1:5) {
             tools::toTitleCase(measure),
             num_cv
             )
+    save(results_models, file = "results/genz/%s/%sDim%sNoSequential%s_%s.RData" %--% c(
+            whichGenz,
+            genzFunctionName,
+            dim,
+            tools::toTitleCase(measure),
+            num_cv
+    )
   } else {
     csvName <- "results/genz/%s/%sDim%s%s_%s.csv" %--% c(
             whichGenz, 
@@ -186,52 +193,23 @@ for (num_cv in 1:5) {
             tools::toTitleCase(measure),
             num_cv
        )
-    figName <- "Figures/%s/%sDim%s%s_%s_.pdf" %--% c(
+    figName <- "Figures/%s/%sDim%s%s_%s.pdf" %--% c(
             whichGenz,
             genzFunctionName,
             dim,
             tools::toTitleCase(measure),
             num_cv
     )
+    save(results_models, file = "results/genz/%s/%sDim%s%s_%s.RData" %--% c(
+            whichGenz,
+            genzFunctionName,
+            dim,
+            tools::toTitleCase(measure),
+            num_cv
+    ))
   }
   
   write.csv(results, file = csvName, row.names=FALSE)
-  
-  print("Begin Plots")
-  # 1. Open jpeg file
-  pdf(figName, width = 10, height = 11)
-  # 2. Create the plot
-  par(mfrow = c(1,2), pty = "s")
-  plot(x = c(1:num_iterations), y = predictionMonteCarlo$meanValueMonteCarlo,
-       pch = 16, type = "l",
-       xlab = "Number of epochs N", ylab = "Integral approximation", col = "blue",
-       main = "Convergence of methods: mean vs N \nusing %s with %s epochs in %s dim" %--% c(genzFunctionName, num_iterations, dim),
-       ylim = c(-real, 3 * real), 
-       lty = 1,
-       xaxs="i", yaxs="i"
-       )
-  lines(x = c(1:num_iterations), predictionBART$meanValueBART, type = 'l', col = "red", lty = 1)
-  lines(x = c(1:num_iterations), predictionGPBQ$meanValueGP, type = 'l', col = "green", lty = 1)
-  abline(a = real, b = 0, lty = 4)
-  legend("topleft", legend=c("MC Integration", "BART BQ", "GP BQ", "Actual"),
-         col=c("blue", "red", "green", "black"), cex=0.8, lty = c(1,1,1,1))
-  
-  # 2. Create the plot
-  plot(x = log(c(2:num_iterations)), y = log(predictionMonteCarlo$standardDeviationMonteCarlo[-1]),
-       pch = 16, type = "l",
-       xlab = "Number of epochs N", ylab = "Log standard deviation", col = "blue",
-       main = "Convergence of methods: log(sd) vs log(N) \nusing %s with %s epochs in %s dim" %--% c(genzFunctionName, num_iterations, dim),
-       lty = 1,
-       xaxs="i", yaxs="i")
-  lines(x = log(c(2:num_iterations)), log(predictionBART$standardDeviationBART[-1]), type = 'l', col = "red", lty = 1)
-  lines(x = log(c(2:num_iterations)), log(sqrt(predictionGPBQ$varianceGP[-1])), type = 'l', col = "green", lty = 1)
-  legend("topleft", legend=c("MC Integration", "BART BQ", "GP BQ"),
-         col=c("blue", "red", "green"), cex=0.8, lty = c(1,1,1,1))
-  # 3. Close the file
-  invisible(dev.off())
-  
-  print("Please check {ROOT}/%s for plots" %--% figName)
-  
 }
 
 
