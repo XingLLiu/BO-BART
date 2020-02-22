fab1d7ba2ecfb54c1cb4f001a40469ac6d75b347
XingLLiu <liuxing971015@outlook.com>
2020-02-22 16:41:02 +0000

    change bobart parameter [22/02/2020]

--- test_nosequential.R
+++ test_nosequential.R
@@ -8,9 +8,3 @@ args <- list(dim = 20, num_iterations = 1, whichGenz = 9, whichKernel = "matern3
              measure = "uniform", epochs = seq(22, 23, by = 10), num_runs = 1)
 
 genz_pipeline_nosequential(args)
-
-plot_args <- list(dims_list = c(20), genz_list = c(9), sequential_list = c("NoSequential"),
-                    measure = "uniform", num_cv = 1)
-plot_results(plot_args)    
-# the current setting in BOBART is correct. See the results for genz1, which seems to be more
-# reasonable      
