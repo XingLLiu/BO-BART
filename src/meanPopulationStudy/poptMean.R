11379b217608db71fd01eac708c497de66572727
hbz <30379161+harrisonzhu508@users.noreply.github.com>
2020-04-22 16:27:08 +0100

    Integrate working survey design code (#82)
    * remove deplicates
    
    * remove subgroup
    
    * remove subgroups, rerun code
    
    * fixed typo
    
    * remove CI
    
    * bug fixed
    
    * remove debug command
    
    * new data set generated, new plot
    
    * deleted plots
    
    * BARTBQ on population data
    
    * fix bug in sample variance
    
    * cv number
    
    * BART-int calculation modified
    
    * typo
    
    * gp on categorical data
    
    * sampling balance
    
    * change length scale
    
    * typo fixed
    
    * gp with categorical data
    
    * standard error
    
    * CI fixed
    
    * modified dataset
    
    * modified dataset
    
    * update function name
    
    * bug fixed
    
    * empirical mean embedding
    
    * empirical mean embedding
    
    * add full sampling
    
    * fix sampling
    
    * add popt mean
    
    * add extraction
    
    * Add files via upload
    
    1. Empirical embedding modified, divided by number of observations
    2. Problem when training length-scale, it fails to stablise 
    3. Problem with GP variance, becomes negative after first variance term is divided by (number of observation)^2.
    
    * remoe extractin
    
    * remove conflicting
    
    * remove redundant file
    
    * add one-hot encoding [21/04/2020]
    
    * change save paths in population study [22/04/2020]
    
    Co-authored-by: RuyaKang <rk2616@ic.ac.uk>
    Co-authored-by: Harrison Zhu <harrisonz@ma-hbz15.ma.ic.ac.uk>
    Co-authored-by: OoAugust <37644581+OoAugust@users.noreply.github.com>
    Co-authored-by: XingLLiu <liuxing971015@outlook.com>

---
+++ src/meanPopulationStudy/poptMean.R
@@ -0,0 +1,150 @@
+library(lhs)
+library(dbarts)
+library(data.tree)
+library(matrixStats)
+library(caret)
+source("src/meanPopulationStudy/bartMean.R")
+
+# paths to save results and plots
+resultPath <- "results/populationStudy/"
+plotPath <- "Figures/populationStudy/"
+
+args <- as.double(commandArgs(TRUE))
+num_new_surveys <- args
+
+# read in data
+trainData <- read.csv("data/train2.csv")
+candidateData <- read.csv("data/candidate2.csv")
+
+# convert num to factor, log income
+convert <- function(data) {
+  
+  log_Total_person_income <- log(data[, ncol(data)])
+  data <- sapply(data[, 2:(ncol(data)-1)], as.factor)
+  data <- data.frame(data)
+  data <- cbind(data, log_Total_person_income)
+}
+
+trainData <- convert(trainData)
+candidateData <- convert(candidateData)
+
+trainData <- trainData[!is.infinite(trainData$log_Total_person_income),]
+candidateData <- candidateData[!is.infinite(candidateData$log_Total_person_income),]
+trainData <- trainData[complete.cases(trainData),]
+candidateData <- candidateData[complete.cases(candidateData),]
+# compute the real population mean log income
+poptMean <- mean(c(trainData$log_Total_person_income, candidateData$log_Total_person_income))
+
+# reconstruct order of the data
+set.seed(2020)
+trainData <- trainData[sample(nrow(trainData)), ]
+candidateData <- candidateData[sample(nrow(candidateData)), ]
+
+# extract covariates and response
+cols <- ncol(trainData)
+trainX <- trainData[, -cols]
+trainY <- trainData[, cols]
+
+candidateX <- candidateData[, -cols]
+candidateY <- candidateData[, cols]
+
+# one-hot encoding
+trainX.num <- trainX
+candidateX.num <- candidateX
+dummyFullData <- dummyVars("~.", data = rbind(trainX, candidateX))
+trainX <- data.frame(predict(dummyFullData, newdata = trainX))
+candidateX <- data.frame(predict(dummyFullData, newdata = candidateX))
+# save(trainX, trainX.num, trainY, candidateX, candidateX.num, candidateY, file = "data/survey_data.RData")
+# load(file = "data/survey_data.RData")
+
+for (num_cv in 1:5) {
+
+    # compute population average income estimates by BARTBQ
+    BARTresults <- computeBART(trainX, trainY, candidateX, candidateY, num_iterations=num_new_surveys)
+    
+    # population average income estimation by Monte Carlo
+    MIresults <- computeMI(trainX.num, trainY, candidateX.num, candidateY, num_iterations=num_new_surveys)
+    
+    # population average income estimation by block random sampling
+    BRSresults <- computeBRS(trainX.num, trainY, candidateX.num, candidateY, group = "Race", num_iterations=num_new_surveys)
+    
+    # store results
+    # results <- data.frame(
+    #     "epochs" = c(1:num_new_surveys),
+    #     "BARTMean" = BARTresults$meanValueBART, "BARTsd" = BARTresults$standardDeviationBART,
+    #     "MIMean" = MIresults$meanValueMI, "MIsd" = MIresults$standardDeviationMI, 
+    #     "BRSMean" = BRSresults$meanValueBRS, "BRSsd" = BRSresults$standardDeviationBRS, 
+    #     "PoptMean" = poptMean, "BpoptMean" = BARTpoptMean
+    # )
+    results <- data.frame(
+        "epochs" = c(1:num_new_surveys),
+        "BARTMean" = BARTresults$meanValueBART, "BARTsd" = BARTresults$standardDeviationBART,
+        "MIMean" = MIresults$meanValueMI, "MIsd" = MIresults$standardDeviationMI, 
+        "BRSMean" = BRSresults$meanValueBRS, "BRSsd" = BRSresults$standardDeviationBRS,
+        "PoptMean" = poptMean
+    )
+    write.csv(results, file = paste0(resultPath, "results", num_cv, ".csv"), row.names=FALSE)
+    
+    real <- results$PoptMean[1]
+    # Breal <- results$BpoptMean[1]
+
+    # 1. Open jpeg file
+    pdf(paste0(plotPath, "results", num_cv, ".pdf"), width = 8, height = 10)
+    par(mfrow = c(1,2), pty = "s")
+    # ymax <- max(c(abs(results$BARTMean - real), abs(results$BRSMean - real), abs(results$MIMean - real)))
+    ymax <- max(c(abs(results$BARTMean - real), abs(results$GPMean - real)))
+    plot(results$epochs,
+         abs(results$BARTMean - results$PoptMean),
+         ty="l",
+         ylab = "Absolute Error",
+         xlab = "num_iterations",
+         col = "orangered",
+         ylim = c(0, ymax)
+    )
+    abline(h=0)
+    
+    points(results$epochs, abs(results$MIMean - real), ty="l", col = "chartreuse4")
+    points(results$epochs, abs(results$BRSMean - real), ty="l", col = "dodgerblue")
+
+    legend("topright", legend=c("Block sampling", "BART-Int", "Monte Carlo sampling"),
+           col=c("dodgerblue", "orangered", "chartreuse4"), cex=0.8, lty = c(1,1,1,1))
+    
+    # ymin <- min(c(results$BARTMean - 2*results$BARTsd, results$BRSMean - 2*results$BRSsd, results$MIMean[1:num_new_surveys]))
+    # ymax <- max(c(results$BARTMean + 2*results$BARTsd, results$BRSMean + 2*results$BRSsd, results$MIMean[1:num_new_surveys]))
+    ymin <- min(c(results$BARTMean - 2*results$BARTsd, results$GPMean - 2*results$GPsd))
+    ymax <- max(c(results$BARTMean + 2*results$BARTsd, results$GPMean + 2*results$GPsd))    
+    
+    plot(results$epochs, 
+         results$MIMean, 
+         ty="l", 
+         ylab = "Mean Population",
+         xlab = "num_iterations",
+         col = "chartreuse4",
+         ylim = c(ymin, ymax)
+    )
+    polygon(c(results$epochs, rev(results$epochs)), 
+            c(
+              results$MIMean + 2*results$MIsd, 
+              rev(results$MIMean - 2*results$MIsd)
+            ), 
+            col = adjustcolor("chartreuse4", alpha.f = 0.10), 
+            border = "chartreuse4", lty = c("dashed", "solid"))
+    points(results$epochs, results$BRSMean, ty="l", col = "dodgerblue")
+    polygon(c(results$epochs, rev(results$epochs)), 
+            c(
+              results$BRSMean + 2*results$BRSsd, 
+              rev(results$BRSMean - 2*results$BRSsd)
+            ), 
+            col = adjustcolor("dodgerblue", alpha.f = 0.10), 
+            border = "dodgerblue", lty = c("dashed", "solid"))
+    points(results$epochs, results$BARTMean, ty="l", col = "orangered")
+    polygon(c(results$epochs, rev(results$epochs)), 
+            c(
+              results$BARTMean + 2*results$BARTsd, 
+              rev(results$BARTMean - 2*results$BARTsd)
+            ), 
+            col = adjustcolor("orangered", alpha.f = 0.10), 
+            border = "orangered", lty = c("dashed", "solid"))
+    abline(h=results$PoptMean)
+    dev.off()
+}
