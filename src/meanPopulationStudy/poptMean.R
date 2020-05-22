93b95f48258070b2b0427e6aef7b14e7b5976a3e
Harrison Zhu <harrisonzhu5080@gmail.com>
2020-05-22 22:09:07 +0100

    standardise the popt code

--- src/meanPopulationStudy/poptMean.R
+++ src/meanPopulationStudy/poptMean.R
@@ -17,7 +17,7 @@ num_new_surveys <- args[1]
 num_cv_start <- args[2]
 num_cv_end <- args[3]
 num_data <- args[4]   # set to 2000 for this lengthscale
-num_design <- 50
+num_design <- args[5]
 
 # read in data
 trainData <- read.csv("data/train2.csv")
@@ -40,9 +40,7 @@ candidateData <- candidateData[!is.infinite(candidateData$log_Total_person_incom
 trainData <- trainData[complete.cases(trainData),]
 candidateData <- candidateData[complete.cases(candidateData),]
 # compute the real population mean log income
-# poptMean <- mean(c(trainData$log_Total_person_income, candidateData$log_Total_person_income))
-
-
+poptMean <- mean(c(trainData$log_Total_person_income, candidateData$log_Total_person_income))
 
 for (num_cv in num_cv_start:num_cv_end) {
   
@@ -51,13 +49,6 @@ for (num_cv in num_cv_start:num_cv_end) {
     print(num_cv)
     trainData <- trainData[sample(c(1:dim(trainData)[1]), num_design),]
     candidateData <- candidateData[1:num_data, ]
-    # poptMean <- mean(c(trainData$log_Total_person_income, candidateData$log_Total_person_income))
-
-    dim <- ncol(trainData)
-    allData <- rbind(trainData, candidateData)
-    model <- bart(allData[,1:(dim-1)], allData[, dim], keeptrees=TRUE, keepevery=3L,
-                nskip=1000, ndpost=10000, ntree=50, k=3, usequant=FALSE)
-    BARTpoptMean <- mean(model$yhat.train.mean)
 
     # # linear regression toy example
     # fullData <- rbind(trainData, candidateData)
@@ -107,12 +98,7 @@ for (num_cv in num_cv_start:num_cv_end) {
     # abline(h = poptMean, col = "red")
     
     # GPBQ
-    if (num_cv == 1) {
-      lengthscale <- optimise_gp_r(as.matrix(trainX), trainY, kernel = "rbf", epochs = 500)
-    } else {
-      lengthscale <- 3.310048
-      # lengthscale <- 3.374
-    }
+    lengthscale <- optimise_gp_r(as.matrix(trainX), trainY, kernel = "rbf", epochs = 500)
     t0 <- proc.time()
     GPresults <- computeGPBQEmpirical(as.matrix(trainX), trainY, as.matrix(candidateX), candidateY, epochs=num_new_surveys, lengthscale=lengthscale)
     t1 <- proc.time()
@@ -123,8 +109,7 @@ for (num_cv in num_cv_start:num_cv_end) {
          "epochs" = c(1:num_new_surveys),
          "BARTMean" = BARTresults$meanValueBART, "BARTsd" = BARTresults$standardDeviationBART,
          "MIMean" = MIresults$meanValueMI, "MIsd" = MIresults$standardDeviationMI, 
-         "GPMean" = GPresults$meanValueGP, "GPsd" = GPresults$varianceGP,
-         "PoptMean" = poptMean, "BpoptMean" = BARTpoptMean,
+         "GPMean" = GPresults$meanValueGP, "GPsd" = GPresults$varianceGP, "PoptMean" = poptMean,
          "runtimeBART" = rep(bartTime, num_new_surveys),
          "runtimeGP" = rep(GPTime, num_new_surveys)
      )
@@ -138,90 +123,13 @@ for (num_cv in num_cv_start:num_cv_end) {
     # )
 	  #results <- data.frame("epochs"=c(1:num_new_surveys), "GPMean"=GPresults$meanValueGP, "GPsd"=GPresults$varianceGP)
     write.csv(results, file = paste0(resultPath, "results", num_cv, ".csv"), row.names=FALSE)
-    results_models <- list("BART"=BARTresults, "MI"=MIresults, "BRS"=BRSresults, "GP"=GPresults)
+    results_models <- list("BART"=BARTresults, "MI"=MIresults, "GP"=GPresults)
     save(results_models, file = paste0(plotPath, "results", num_cv, ".RData"))
     
-    #results_models <- list("GP"=GPresults)
-  	#save(results_models, file = paste0(plotPath, "gpresults", num_cv, ".RData"))
-    
     real <- results$PoptMean[1]
-    Breal <- results$BpoptMean[1]
     
-    print(c("Real BART-Int: ", Breal))
     print(c("Real MC: ", real))
     print(c("BART-Int: ", results$BARTMean[num_new_surveys]))
     print(c("GP-BQ: ", results$GPMean[num_new_surveys]))
     print(c("MI: ", results$MIMean[num_new_surveys]))
-    
-    # 1. Open jpeg file
-    pdf(paste0(plotPath, "results", num_cv, ".pdf"), width = 8, height = 10)
-    par(mfrow = c(1,2), pty = "s")
-    # ymax <- max(c(abs(results$BARTMean - real), abs(results$BRSMean - real), abs(results$MIMean - real)))
-    ymax <- max(c(abs(results$BARTMean - real), abs(results$GPMean - real)))
-    plot(results$epochs,
-         abs(results$BARTMean - results$PoptMean),
-         ty="l",
-         ylab = "Absolute Error",
-         xlab = "num_iterations",
-         col = "orangered",
-         ylim = c(0, ymax)
-    )
-    abline(h=0)
-    
-    points(results$epochs, abs(results$MIMean - real), ty="l", col = "chartreuse4")
-    points(results$epochs, abs(results$BRSMean - real), ty="l", col = "dodgerblue")
-    points(results$epochs, abs(results$GPMean - real), ty="l", col = "darkgoldenrod")
-
-    legend("topright", legend=c("Block sampling", "BART-Int", "Monte Carlo sampling", "GPBQ"),
-           col=c("dodgerblue", "orangered", "chartreuse4", "darkgoldenrod"), cex=0.8, lty = c(1,1,1,1))
-    
-    # ymin <- min(c(results$BARTMean - 2*results$BARTsd, results$BRSMean - 2*results$BRSsd, results$MIMean[1:num_new_surveys]))
-    # ymax <- max(c(results$BARTMean + 2*results$BARTsd, results$BRSMean + 2*results$BRSsd, results$MIMean[1:num_new_surveys]))
-    ymin <- min(c(results$BARTMean - 2*results$BARTsd, results$GPMean - 2*results$GPsd))
-    ymax <- max(c(results$BARTMean + 2*results$BARTsd, results$GPMean + 2*results$GPsd))    
-    
-    plot(results$epochs, 
-         results$MIMean, 
-         ty="l", 
-         ylab = "Mean Population",
-         xlab = "num_iterations",
-         col = "chartreuse4",
-         ylim = c(ymin, ymax)
-    )
-    polygon(c(results$epochs, rev(results$epochs)), 
-            c(
-              results$MIMean + 2*results$MIsd, 
-              rev(results$MIMean - 2*results$MIsd)
-            ), 
-            col = adjustcolor("chartreuse4", alpha.f = 0.10), 
-            border = "chartreuse4", lty = c("dashed", "solid"))
-    points(results$epochs, results$BRSMean, ty="l", col = "dodgerblue")
-    polygon(c(results$epochs, rev(results$epochs)), 
-            c(
-              results$BRSMean + 2*results$BRSsd, 
-              rev(results$BRSMean - 2*results$BRSsd)
-            ), 
-            col = adjustcolor("dodgerblue", alpha.f = 0.10), 
-            border = "dodgerblue", lty = c("dashed", "solid"))
-    points(results$epochs, results$BARTMean, ty="l", col = "orangered")
-    polygon(c(results$epochs, rev(results$epochs)), 
-            c(
-              results$BARTMean + 2*results$BARTsd, 
-              rev(results$BARTMean - 2*results$BARTsd)
-            ), 
-            col = adjustcolor("orangered", alpha.f = 0.10), 
-            border = "orangered", lty = c("dashed", "solid"))
-    
-    points(results$epochs, results$GPMean, ty="l", col = "darkgoldenrod")
-    polygon(c(results$epochs, rev(results$epochs)), 
-            c(
-              results$GPMean + 2*results$GPsd, 
-              rev(results$GPMean - 2*results$GPsd)
-            ), 
-            col = adjustcolor("darkgoldenrod", alpha.f = 0.10), 
-            border = "darkgoldenrod", lty = c("dashed", "solid"))
-    
-    abline(h=results$PoptMean)
-    dev.off()
-	
 }
